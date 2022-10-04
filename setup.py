2237ca83f2b3dd762d6443e842cc24ab28520ab5
sandybradley <sandy.bradley@gmail.com>
2022-10-04 23:06:31 +0200

    chore: update dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = open('README.md').read()
 
 setup(
     name='aio-eth',
-    version='0.0.1',
+    version='0.0.2',
     author='Narasimha Prasanna HN',
     author_email='narasimhaprasannahn@gmail.com',
     url='https://github.com/Narasimha1997/pyMigrate',
@@ -25,6 +25,6 @@ setup(
     keywords='python web3 ethereum blockchain asyncio aio',
     zip_safe=False,
     install_requires=[
-        "aiohttp>=3.7.4.post0"
+        "aiohttp>=3.8.3"
     ]
 )
